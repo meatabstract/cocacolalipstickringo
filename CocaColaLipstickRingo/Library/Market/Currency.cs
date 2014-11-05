ce2af6058f3cc3d508213d035113e0a2f9a932df
mavarok <mavarok@mavarok.com>
2014-11-05 13:45:07 +0000

    Stuff

---
+++ CocaColaLipstickRingo/Library/Market/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCLR.Market
+{
+	public enum Currency
+	{
+		Bitcoin = 0,
+		Litecoin = 1,
+		DogeCoin = 2
+	}
+}
